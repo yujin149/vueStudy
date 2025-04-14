9b848ecc2ef6bb69af3f409b337b539946279a79
yujin149 <swtask149@gmail.com>
2025-04-14 20:43:54 +0900

    vue3 master - Nested Routes

---
+++ vue3-master/04-NestedRoutes/src/components/posts/PostItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">
+        {{ content }}
+      </p>
+      <p class="text-muted">{{ createdAt }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title : {
+    type:String,
+    required : true,
+  },
+  content : {
+    type:String,
+  },
+  createdAt : {
+    type:[String, Date, Number],
+  }
+})
+</script>
+
+<style scoped>
+.card {cursor: pointer;}
+</style>
---
+++ vue3-master/04-NestedRoutes/src/views/posts/PostDetailView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+      <h2>제목</h2>
+      <p>내용</p>
+      <p class="text-muted">2025-01-01</p>
+      <hr class="my-4" />
+      <div class="row g-2">
+        <div class="col-auto">
+          <button class="btn btn-outline-dark">이전글</button>
+        </div>
+        <div class="col-auto">
+          <button class="btn btn-outline-dark">다음글</button>
+        </div>
+        <div class="col-auto me-auto"></div>
+        <div class="col-auto">
+          <button class="btn btn-outline-dark" @click="goListPage">목록</button>
+        </div>
+        <div class="col-auto">
+          <button class="btn btn-outline-primary" @click="goEditPage">수정</button>
+        </div>
+        <div class="col-auto">
+          <button class="btn btn-outline-danger">삭제</button>
+        </div>
+      </div>
+
+      <!-- /posts/1 으로 접속했을때, (1부분은 임의로 넣음) -->
+      <!-- <p>params : {{$route.params}}</p> -->
+      
+      <!-- /posts/1?searchText=vue3바이블 -->
+      <!-- <p>query : {{$route.query.searchText}}</p> -->
+
+      <!-- /posts/1#hashvalue -->
+      <!-- <p>hash : {{$route.hash}}</p> -->
+
+
+
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+
+
+const route = useRoute();
+const id = route.params.id;
+
+const router = useRouter();
+const goListPage = () => {
+  router.push({
+    name:'PostList',
+  });
+}
+const goEditPage = () => {
+  router.push({
+    name:'PostEdit',
+    params: {id},
+  });
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

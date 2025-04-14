9e6e38e2a70485482eae3528ee884a470e543556
yujin149 <swtask149@gmail.com>
2025-04-14 20:38:28 +0900

    vue3 basic

---
+++ vue3-basic/SECTION05/vue3-dynamicComponents/src/components/AppCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <div class="card-header" v-if="$slots.header"><slot name="header" header-message="헤더 메세지"></slot></div>
+    <!-- {{ $slots }} -->
+    <div class="card-body" v-if="$slots.default">
+      <!--<slot>#Body</slot>  이름을 주지 않게 되면 default라는 이름이 자동으로 붙는다.-->
+      <slot :child-message="childMessage" hello-message="안녕하세요"></slot> 
+    </div>
+    <div v-if="hasFooter" class="card-footer text-muted"><slot name="footer" footer-message="푸터 메세지"></slot></div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+  setup(props, { slots }) {
+    const childMessage = ref('자식컴포넌트 메세지');
+    //context.slots
+    const hasFooter = computed(() => !!slots.footer);
+    return {childMessage, hasFooter};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ vue3-basic/SECTION05/vue3-dynamicComponents/src/components/DynamicComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container py-4">
+        <button class="btn btn-primary me-2" @click="changeCurrentComp(DynamicApple)">사과</button>
+        <button class="btn btn-danger" @click="changeCurrentComp(DynamicBanana)">바나나</button>
+        <hr />
+        <component :is="currentComp"></component>
+
+        <p>{{obj1}}</p>
+        <p>{{obj2}}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { shallowRef } from "vue";
+import DynamicApple from "./DynamicApple.vue";
+import DynamicBanana from "./DynamicBanana.vue";
+
+//const currentComp = ref(DynamicApple);
+// is 속성으로 컴포넌트를 넘길 때는 shallowRef로 정의하는게 좋음.
+const currentComp = shallowRef(DynamicApple);
+
+const obj1 = ref({name:'홍길동'});
+const obj2 = shallowRef({name:'김길동'});
+ // shallowRef는 속성에 대해서는 반응하지 않고 값 자체가 바뀌었을 때만 반응.
+ //obj2.value 이 바뀌었을때, 반응적. 
+ //obj2.value.name 이 바뀌었을때, 반응적이지 않다. 
+
+
+const changeCurrentComp = (comp) => {currentComp.value = comp};
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
